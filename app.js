030826c2c6717c3f017fb9e3cb303d811f5df0d8
Nick <nwharr12@gmail.com>
2021-09-20 14:14:53 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const addSquare = document.createElement("button");
+addSquare.textContent = "Add Square";
+document.body.appendChild(addSquare)
+
+let div = document.createElement("div")
+div.appendChild = "Black Square"
+// ?????
+
+
+
+
+
+
+
+
